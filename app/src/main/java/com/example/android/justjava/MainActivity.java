4e2e17501f88faa9fcfa2774371049c935fb910c
EnduranceCode <endurancecode@gmail.com>
2018-01-18 23:45:50 +0000

    Chapter 14 - Quiz: Hook Up Two Buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -28,10 +28,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 6;
+        int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
     }
