0e160417aab60d60dafceb0d20c6f555147bf349
EnduranceCode <endurancecode@gmail.com>
2018-01-19 10:01:49 +0000

    Chapter 21 - Quiz: Make the Quantity Picker Work

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,6 +21,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        int quantity = 2;
         quantity = quantity + 1;
         display(quantity);
         displayPrice(quantity * 5);
@@ -41,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        int quantity = 2;
         quantity = quantity - 1;
         display(quantity);
         displayPrice(quantity * 5);
