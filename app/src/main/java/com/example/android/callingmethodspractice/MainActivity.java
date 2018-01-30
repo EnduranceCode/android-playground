839dd2261160a6714c4cf1ede2a896c4bf00ccbc
EnduranceCode <endurancecode@gmail.com>
2018-01-30 09:20:10 +0000

    Experiment calling methods on TextView

--- app/src/main/java/com/example/android/callingmethodspractice/MainActivity.java
+++ app/src/main/java/com/example/android/callingmethodspractice/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import static android.graphics.Color.RED;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView textView = new TextView(this);
-        textView.setText("Wow!");
+        textView.setText("Determination");
+        textView.setTextColor(RED);
+        textView.setTextSize(32);
         setContentView(textView);
     }
 }
