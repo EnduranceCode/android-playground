fa1f9de45313c8e156c7a1710e3f8b17079917d6
EnduranceCode <endurancecode@gmail.com>
2018-06-12 13:14:19 +0100

    Task: Create TextView

--- app/src/main/java/com/endurancecode/PracticeSetObjectOrientedProgramming/MainActivity.java
+++ app/src/main/java/com/endurancecode/PracticeSetObjectOrientedProgramming/MainActivity.java
@@ -2,6 +2,7 @@ package com.endurancecode.PracticeSetObjectOrientedProgramming;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        TextView country = (TextView) findViewById(R.id.country);
+
+        name.setText("Ricardo");
+        birthday.setText("06-02-1971");
+        country.setText("Portugal");
     }
 }
