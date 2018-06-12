a228babaea2c48363c070a5e9e99b22deb611826
EnduranceCode <endurancecode@gmail.com>
2018-06-12 13:24:23 +0100

    Task: Add ImageView

--- app/src/main/java/com/endurancecode/PracticeSetObjectOrientedProgramming/MainActivity.java
+++ app/src/main/java/com/endurancecode/PracticeSetObjectOrientedProgramming/MainActivity.java
@@ -2,6 +2,7 @@ package com.endurancecode.PracticeSetObjectOrientedProgramming;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView profile_picture = findViewById(R.id.profile_picture);
         TextView name = (TextView) findViewById(R.id.name);
         TextView birthday = (TextView) findViewById(R.id.birthday);
         TextView country = (TextView) findViewById(R.id.country);
 
+        profile_picture.setImageResource(R.drawable.avatar);
         name.setText("Ricardo");
         birthday.setText("06-02-1971");
         country.setText("Portugal");
