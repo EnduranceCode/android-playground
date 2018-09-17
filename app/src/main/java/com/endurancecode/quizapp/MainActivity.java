9bccf09c2ed9a45d83269ea042feb5389895de1f
EnduranceCode <endurancecode@gmail.com>
2018-09-17 14:30:28 +0100

    Save variables state on screen rotation
    Follow method explained in Google Challenge Scholarship: Android Basics
    Forum:
    https://discussions.udacity.com/t/button-changed-position-and-scores-set-to-zero-when-switching-between-portrait-and-landscape-orientations/466340

--- app/src/main/java/com/endurancecode/quizapp/MainActivity.java
+++ app/src/main/java/com/endurancecode/quizapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.endurancecode.quizapp;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String STATE_SCORE = "score";
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current score state
+        savedInstanceState.putInt(STATE_SCORE, score);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore state members from saved instance
+        score = savedInstanceState.getInt(STATE_SCORE);
     }
 }
