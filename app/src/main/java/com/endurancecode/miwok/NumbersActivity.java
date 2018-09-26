b6836de9bf71f262ccf5b4050e72fb197fb225aa
EnduranceCode <endurancecode@gmail.com>
2018-09-26 16:14:03 +0100

    Verify array elements with log messages

--- app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
+++ app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
@@ -1,7 +1,8 @@
 package com.endurancecode.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -21,5 +22,16 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumbers[7] = "eight";
         englishNumbers[8] = "nine";
         englishNumbers[9] = "ten";
+
+        Log.v("NumbersActivity", "englishNumbers[0]: " + englishNumbers[0]);
+        Log.v("NumbersActivity", "englishNumbers[1]: " + englishNumbers[1]);
+        Log.v("NumbersActivity", "englishNumbers[2]: " + englishNumbers[2]);
+        Log.v("NumbersActivity", "englishNumbers[3]: " + englishNumbers[3]);
+        Log.v("NumbersActivity", "englishNumbers[4]: " + englishNumbers[4]);
+        Log.v("NumbersActivity", "englishNumbers[5]: " + englishNumbers[5]);
+        Log.v("NumbersActivity", "englishNumbers[6]: " + englishNumbers[6]);
+        Log.v("NumbersActivity", "englishNumbers[7]: " + englishNumbers[7]);
+        Log.v("NumbersActivity", "englishNumbers[8]: " + englishNumbers[8]);
+        Log.v("NumbersActivity", "englishNumbers[9]: " + englishNumbers[9]);
     }
 }
