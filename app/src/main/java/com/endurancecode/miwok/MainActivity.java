b3242a470aa0fcf69ec8d5a228b71d922ad71c49
EnduranceCode <endurancecode@gmail.com>
2018-09-26 10:11:05 +0100

    Use OnClickListeners for all categories

--- app/src/main/java/com/endurancecode/miwok/MainActivity.java
+++ app/src/main/java/com/endurancecode/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +29,65 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view) {
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
+        // Get the Colors Text View
+        TextView colorsView = (TextView) findViewById(R.id.colors);
+
+        // Set OnClickListener method for Colors View
+        colorsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create Intent to open the Colors Activity
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+
+                // Start the colorsIntent (Colors Activity)
+                startActivity(colorsIntent);
+            }
+        });
+
+        // Get the Family Text View
+        TextView familyView = (TextView) findViewById(R.id.family);
+
+        // Set OnClickListener method for Family View
+        familyView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create Intent to open the Family Activity
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+
+                // Start the numbersIntent (Numbers Activity)
+                startActivity(familyIntent);
+            }
+        });
+
+        // Get the Number Text View
+        TextView numbersView = (TextView) findViewById(R.id.numbers);
+
+        // Set OnClickListener method for Numbers View
+        numbersView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create Intent to open the Numbers Activity
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+
+                // Start the numbersIntent (Numbers Activity)
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Get the Phrases Text View
+        TextView phrasesView = (TextView) findViewById(R.id.phrases);
+
+        // Set OnClickListener method for Phrases View
+        phrasesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create Intent to open the Phrases Activity
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+
+                // Start the phrasesIntent (Phrases Activity)
+                startActivity(phrasesIntent);
+            }
+        });
     }
 }
