903e4934b60cd5e30cd0433f40792012647280a3
EnduranceCode <endurancecode@gmail.com>
2018-09-26 12:16:49 +0100

    Add words to NumbersActivity

--- app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
+++ app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
@@ -9,5 +9,17 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] englishNumbers = new String[10];
+        englishNumbers[0] = "one";
+        englishNumbers[1] = "two";
+        englishNumbers[2] = "three";
+        englishNumbers[3] = "four";
+        englishNumbers[4] = "five";
+        englishNumbers[5] = "six";
+        englishNumbers[6] = "seven";
+        englishNumbers[7] = "eight";
+        englishNumbers[8] = "nine";
+        englishNumbers[9] = "ten";
     }
 }
