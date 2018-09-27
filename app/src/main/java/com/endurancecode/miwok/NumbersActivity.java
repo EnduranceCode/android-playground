54fdb9458fdd493e474ba02d44a8298f40b0ac6c
EnduranceCode <endurancecode@gmail.com>
2018-09-27 10:15:24 +0100

    Add TextViews to the Layout Based on the List

--- app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
+++ app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
@@ -3,6 +3,9 @@ package com.endurancecode.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,6 +16,7 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+        //Create ArrayList to store english words for numbers
         ArrayList<String> englishNumbers = new ArrayList<String>();
 
         englishNumbers.add("one");
@@ -26,15 +30,48 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumbers.add("nine");
         englishNumbers.add("ten");
 
-        Log.v("NumbersActivity", "englishNumbers[0]: " + englishNumbers.get(0));
-        Log.v("NumbersActivity", "englishNumbers[1]: " + englishNumbers.get(1));
-        Log.v("NumbersActivity", "englishNumbers[2]: " + englishNumbers.get(2));
-        Log.v("NumbersActivity", "englishNumbers[3]: " + englishNumbers.get(3));
-        Log.v("NumbersActivity", "englishNumbers[4]: " + englishNumbers.get(4));
-        Log.v("NumbersActivity", "englishNumbers[5]: " + englishNumbers.get(5));
-        Log.v("NumbersActivity", "englishNumbers[6]: " + englishNumbers.get(6));
-        Log.v("NumbersActivity", "englishNumbers[7]: " + englishNumbers.get(7));
-        Log.v("NumbersActivity", "englishNumbers[8]: " + englishNumbers.get(8));
-        Log.v("NumbersActivity", "englishNumbers[9]: " + englishNumbers.get(9));
+        // Find the root view in NumbersActivity
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // Add the TextViews with the english words for numbers to the Numbers layout
+        TextView wordView0 = new TextView(this);
+        wordView0.setText(englishNumbers.get(0));
+        rootView.addView(wordView0);
+
+        TextView wordView1 = new TextView(this);
+        wordView1.setText(englishNumbers.get(1));
+        rootView.addView(wordView1);
+
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(englishNumbers.get(2));
+        rootView.addView(wordView2);
+
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(englishNumbers.get(3));
+        rootView.addView(wordView3);
+
+        TextView wordView4 = new TextView(this);
+        wordView4.setText(englishNumbers.get(4));
+        rootView.addView(wordView4);
+
+        TextView wordView5 = new TextView(this);
+        wordView5.setText(englishNumbers.get(5));
+        rootView.addView(wordView5);
+
+        TextView wordView6 = new TextView(this);
+        wordView6.setText(englishNumbers.get(6));
+        rootView.addView(wordView6);
+
+        TextView wordView7 = new TextView(this);
+        wordView7.setText(englishNumbers.get(7));
+        rootView.addView(wordView7);
+
+        TextView wordView8 = new TextView(this);
+        wordView8.setText(englishNumbers.get(8));
+        rootView.addView(wordView8);
+
+        TextView wordView9 = new TextView(this);
+        wordView9.setText(englishNumbers.get(9));
+        rootView.addView(wordView9);
     }
 }
