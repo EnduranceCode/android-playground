6867cb97feb3c9be49ea9a3235adf3f0ae31522a
EnduranceCode <endurancecode@gmail.com>
2018-09-27 09:27:18 +0100

    Switch from Array to ArrayList

--- app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
+++ app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,27 +13,28 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] englishNumbers = new String[10];
-        englishNumbers[0] = "one";
-        englishNumbers[1] = "two";
-        englishNumbers[2] = "three";
-        englishNumbers[3] = "four";
-        englishNumbers[4] = "five";
-        englishNumbers[5] = "six";
-        englishNumbers[6] = "seven";
-        englishNumbers[7] = "eight";
-        englishNumbers[8] = "nine";
-        englishNumbers[9] = "ten";
+        ArrayList<String> englishNumbers = new ArrayList<String>();
 
-        Log.v("NumbersActivity", "englishNumbers[0]: " + englishNumbers[0]);
-        Log.v("NumbersActivity", "englishNumbers[1]: " + englishNumbers[1]);
-        Log.v("NumbersActivity", "englishNumbers[2]: " + englishNumbers[2]);
-        Log.v("NumbersActivity", "englishNumbers[3]: " + englishNumbers[3]);
-        Log.v("NumbersActivity", "englishNumbers[4]: " + englishNumbers[4]);
-        Log.v("NumbersActivity", "englishNumbers[5]: " + englishNumbers[5]);
-        Log.v("NumbersActivity", "englishNumbers[6]: " + englishNumbers[6]);
-        Log.v("NumbersActivity", "englishNumbers[7]: " + englishNumbers[7]);
-        Log.v("NumbersActivity", "englishNumbers[8]: " + englishNumbers[8]);
-        Log.v("NumbersActivity", "englishNumbers[9]: " + englishNumbers[9]);
+        englishNumbers.add("one");
+        englishNumbers.add("two");
+        englishNumbers.add("three");
+        englishNumbers.add("four");
+        englishNumbers.add("five");
+        englishNumbers.add("six");
+        englishNumbers.add("seven");
+        englishNumbers.add("eight");
+        englishNumbers.add("nine");
+        englishNumbers.add("ten");
+
+        Log.v("NumbersActivity", "englishNumbers[0]: " + englishNumbers.get(0));
+        Log.v("NumbersActivity", "englishNumbers[1]: " + englishNumbers.get(1));
+        Log.v("NumbersActivity", "englishNumbers[2]: " + englishNumbers.get(2));
+        Log.v("NumbersActivity", "englishNumbers[3]: " + englishNumbers.get(3));
+        Log.v("NumbersActivity", "englishNumbers[4]: " + englishNumbers.get(4));
+        Log.v("NumbersActivity", "englishNumbers[5]: " + englishNumbers.get(5));
+        Log.v("NumbersActivity", "englishNumbers[6]: " + englishNumbers.get(6));
+        Log.v("NumbersActivity", "englishNumbers[7]: " + englishNumbers.get(7));
+        Log.v("NumbersActivity", "englishNumbers[8]: " + englishNumbers.get(8));
+        Log.v("NumbersActivity", "englishNumbers[9]: " + englishNumbers.get(9));
     }
 }
