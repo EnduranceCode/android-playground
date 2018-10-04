9aae70372e5bd73c05be6f056c60c8e0dd0f872f
EnduranceCode <endurancecode@gmail.com>
2018-10-04 10:32:44 +0100

    Experiment using a GridView and Adapter Quiz

--- app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
+++ app/src/main/java/com/endurancecode/miwok/NumbersActivity.java
@@ -2,12 +2,8 @@ package com.endurancecode.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 
@@ -44,7 +40,7 @@ public class NumbersActivity extends AppCompatActivity {
          * There should be a {@link ListView} with the view ID called list, which is declared in the
          * activity_numbers.xml layout file.
          */
-        ListView listView = (ListView) findViewById(R.id.list);
+        GridView listView = (GridView) findViewById(R.id.list);
 
         /* Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
          * {@link ListView} will display list items for each word in the list of words.
