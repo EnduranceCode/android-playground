51272a0ba34f30e5fbd86cc8dd6e2a86ca1334bb
EnduranceCode <endurancecode@gmail.com>
2018-10-04 12:18:54 +0100

    Create custom Word class

---
+++ app/src/main/java/com/endurancecode/miwok/Word.java
@@ -0,0 +1,39 @@
+package com.endurancecode.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+    /**
+     *  Define the string variables that store the different languages translations
+     */
+
+    // Variable that stores the default translation word
+    private String mDefaultTranslation;
+
+    // Variable that stores Miwok translation word
+    private String mMiwokTranslation;
+
+    /**
+     * Constructs a new Word with initial values for default translation and Miwok translation
+     */
+    public Word (String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = "defaultTranslation";
+        mMiwokTranslation = "miwokTranslation";
+    }
+
+    /**
+     * Methods to get the string variables that store the different languages translations
+     */
+
+    // Method to get the default translation
+    public String getDefaultTranslation () {
+        return mDefaultTranslation;
+    }
+
+    // Method to get the Miwok translation
+    public String getMiwokTranslation () {
+        return mMiwokTranslation;
+    }
+}
