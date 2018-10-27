9ad0503cb813bd90df37f78ef9ce347622717850
EnduranceCode <endurancecode@gmail.com>
2018-10-27 17:35:25 +0100

    Add Songs List Activity
    Renames activity_main.xml to songs_list.xml and MainActivity.java to
    SongsListActivity.java

--- app/src/main/java/com/endurancecode/musicapp/SongsListActivity.java
+++ app/src/main/java/com/endurancecode/musicapp/SongsListActivity.java
@@ -3,11 +3,16 @@ package com.endurancecode.musicapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SongsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.songs_list);
+
+        // Hide Action Bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
