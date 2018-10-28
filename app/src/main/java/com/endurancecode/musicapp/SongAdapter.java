100c4834429c3c60ed7a8eaa10359fa7bbc167fb
EnduranceCode <endurancecode@gmail.com>
2018-10-28 20:30:32 +0000

    Add an ArrayAdapter to display the song's list

---
+++ app/src/main/java/com/endurancecode/musicapp/SongAdapter.java
@@ -0,0 +1,77 @@
+package com.endurancecode.musicapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * {@link SongAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
+ * based on a data source, which is a list of {@link Song} objects.
+ */
+public class SongAdapter extends ArrayAdapter<Song> {
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param songs   A List of Song objects to display in a list
+     */
+    public SongAdapter(Activity context, ArrayList<Song> songs) {
+        /* Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+         * the second argument is used when the ArrayAdapter is populating a single TextView.
+         * Because this is a custom adapter for two TextViews and two ImageViews, the adapter is not
+         * going to use this second argument, so it can be any value. Here, we used 0.
+         */
+        super(context, 0, songs);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The position in the list of data that should be displayed in the
+     *                    song item list view.
+     * @param convertView The recycled view to populate.
+     * @param parent      The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View songItemListView = convertView;
+        if (songItemListView == null) {
+            songItemListView = LayoutInflater.from(getContext()).inflate(R.layout.song_item_list, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Song currentSong = getItem(position);
+
+        // Find the ImageView in the song_item_list.xml layout with the ID songItemCoverArt
+        ImageView coverArtImageView = (ImageView) songItemListView.findViewById(R.id.songItemCoverArt);
+        // Get the album cover art from the current Song object and set it on the cover art ImageView
+        coverArtImageView.setImageResource(currentSong.getSongCoverArt());
+
+        // Find the TextView in the song_item_list.xml layout with the ID songItemTitle
+        TextView songTitleTextView = (TextView) songItemListView.findViewById(R.id.songItemTitle);
+        // Get the song's title from the current Song object and set this text on the song title TextView
+        songTitleTextView.setText(currentSong.getSongTitle());
+
+        // Find the TextView in the song_item_list.xml layout with the ID
+        TextView songArtistTextView = (TextView) songItemListView.findViewById(R.id.songItemArtist);
+        // Get the song's artist from the current Song object and set this text on the song artist TextView
+        songArtistTextView.setText(currentSong.getSongArtist());
+
+        // Return the whole list item layout (containing 2 TextViews)
+        // so that it can be shown in the ListView
+        return songItemListView;
+    }
+}
--- app/src/main/java/com/endurancecode/musicapp/SongsListActivity.java
+++ app/src/main/java/com/endurancecode/musicapp/SongsListActivity.java
@@ -2,6 +2,9 @@ package com.endurancecode.musicapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SongsListActivity extends AppCompatActivity {
 
@@ -14,5 +17,61 @@ public class SongsListActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+
+        /* Create an ArrayList that stores objects of type Song.
+         *
+         * This ArrayList is created with 12 objects and the information used is collected in the
+         * following Wikipedia Pages:
+         *  https://en.wikipedia.org/wiki/War_(U2_album)#Track_listing
+         *  https://en.wikipedia.org/wiki/Under_a_Blood_Red_Sky
+         *  https://en.wikipedia.org/wiki/Come_On_Pilgrim
+         *  https://en.wikipedia.org/wiki/Surfer_Rosa
+         *  https://en.wikipedia.org/wiki/Doolittle_(album)
+         *  https://en.wikipedia.org/wiki/Nevermind
+         *  https://en.wikipedia.org/wiki/In_Utero_(album)
+         *
+         * The copyright information of images of the album's cover art is stated in the following url's:
+         * u2_war.jpg				    https://en.wikipedia.org/wiki/File:U2_War_album_cover.jpg
+         * u2_under_a_blood_red_sky.jpg https://en.wikipedia.org/wiki/File:U2uabrs.jpg
+         * pixies_come_on_pilgrim.jpg   https://en.wikipedia.org/wiki/File:Come_on_pilgrim.jpg
+         * pixies_surfer_rosa.jpg	    https://en.wikipedia.org/wiki/File:SurferRosa.jpg
+         * pixies_doolittle.jpg		    https://en.wikipedia.org/wiki/File:Pixies-Doolittle.jpg
+         * Nirvana_nevermind.jpg	    https://en.wikipedia.org/wiki/File:NirvanaNevermindalbumcover.jpg
+         * Nirvana_in_utero.jpg		    https://en.wikipedia.org/wiki/File:In_Utero_(Nirvana)_album_cover.jpg
+         */
+        ArrayList<Song> songs = new ArrayList<>();
+
+        songs.add(new Song("Sunday bloody sunday", "U2", "War", R.drawable.u2_war, 1983, "Rock", "4:38"));
+        songs.add(new Song("New year's day", "U2", "War", R.drawable.u2_war, 1983, "Rock", "5:38"));
+        songs.add(new Song("11 o'clock tick tock", "U2", "Under a blood red sky", R.drawable.u2_under_a_blood_red_sky, 1983, "Rock", "4:34"));
+        songs.add(new Song("Party Girl", "U2", "Under a Blood Red Sky", R.drawable.u2_under_a_blood_red_sky, 1983, "Rock", "4:34"));
+        songs.add(new Song("Vamos", "Pixies", "Come on Pilgrim", R.drawable.pixies_come_on_pilgrim, 1987, "Rock", "2:53"));
+        songs.add(new Song("Where is my mind", "Pixies", "Surfer Rosa", R.drawable.pixies_surfer_rosa, 1988, "Rock", "3:53"));
+        songs.add(new Song("Catus", "Pixies", "Surfer Rosa", R.drawable.pixies_surfer_rosa, 1988, "Rock", "2:16"));
+        songs.add(new Song("Here comes your man", "Pixies", "Doolittle", R.drawable.pixies_doolittle, 1989, "Rock", "3:21"));
+        songs.add(new Song("Smells like teen spirit", "Nirvana", "Nevermind", R.drawable.nirvana_nevermind, 1991, "Rock", "5:01"));
+        songs.add(new Song("Come as you are", "Nirvana", "Nevermind", R.drawable.nirvana_nevermind, 1991, "Rock", "3:39"));
+        songs.add(new Song("In bloom", "Nirvana", "Nevermind", R.drawable.nirvana_nevermind, 1991, "Rock", "4:14"));
+        songs.add(new Song("Heart shaped box", "Nirvana", "Nevermind", R.drawable.nirvana_in_utero, 1993, "Rock", "4:41"));
+
+        /* Create a custom {@link SongAdapter}, whose data source is a list of Song objects. The
+         * adapter uses song_item_list.xml layout to create layouts for each item in the list.
+         * This list item layout contains two {@link TextView} and two {@link ImageView, which the
+         * adapter will set to display some of the data of each Song object in the ArrayList songs.
+         */
+        SongAdapter wordAdapter = new SongAdapter(this, songs);
+
+        /* Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+         * There should be a {@link ListView} with the view ID called list, which is declared in the
+         * word_list.xml file.
+         */
+        ListView listView = (ListView) findViewById(R.id.songsList);
+
+        /* Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+         * {@link ListView} will display list items for each word in the list of words.
+         * Do this by calling the setAdapter method on the {@link ListView} object and pass in
+         */
+        listView.setAdapter(wordAdapter);
     }
 }
