235bad2007c09e804d8c75afdbd43008b7e6ac2e
EnduranceCode <endurancecode@gmail.com>
2018-11-03 23:20:04 +0000

    Add music to raw folder
    Music attribution:
    Cha Cappella by Jimmy Fontanez/Media Right Productions
    Downloaded from https://www.youtube.com/audiolibrary/music

--- app/src/main/java/com/endurancecode/SampleMusicPlayer/MainActivity.java
+++ app/src/main/java/com/endurancecode/SampleMusicPlayer/MainActivity.java
@@ -3,6 +3,11 @@ package com.endurancecode.SampleMusicPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/* Music attribution:
+ * Cha Cappella by Jimmy Fontanez/Media Right Productions
+ * Downloaded from https://www.youtube.com/audiolibrary/music
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
