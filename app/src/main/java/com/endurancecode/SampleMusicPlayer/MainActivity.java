24aa37584bdd32adeb965405efd4f5d8d0cc049a
EnduranceCode <endurancecode@gmail.com>
2018-11-04 19:01:12 +0000

    Add expected functionality to the UI buttons

--- app/src/main/java/com/endurancecode/SampleMusicPlayer/MainActivity.java
+++ app/src/main/java/com/endurancecode/SampleMusicPlayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.endurancecode.SampleMusicPlayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /* Music attribution:
  * Cha Cappella by Jimmy Fontanez/Media Right Productions
@@ -14,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Create MediaPlayer object with cha_cappella.mp3 */
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.cha_cappella);
+
+        /* Find buttons UI buttons */
+        Button btnPlay = (Button) findViewById(R.id.button_play);
+        Button btnPause = (Button) findViewById(R.id.button_pause);
+
+        /* Set OnClickListener method to the play button */
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        /* Set OnClickListener method to the pause button */
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
     }
 }
