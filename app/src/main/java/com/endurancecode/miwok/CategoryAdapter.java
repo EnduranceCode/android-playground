c1fe99b32fe8b279c5af65af74972d23fc5de93e
EnduranceCode <endurancecode@gmail.com>
2018-11-12 15:16:40 +0000

    Add Tabs to ViewPager

--- app/src/main/java/com/endurancecode/miwok/CategoryAdapter.java
+++ app/src/main/java/com/endurancecode/miwok/CategoryAdapter.java
@@ -1,12 +1,27 @@
 package com.endurancecode.miwok;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-    public CategoryAdapter(FragmentManager fragmentManager) {
+
+    /* Context of the app */
+    private Context mContextFragment;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param context         is the context of the app
+     * @param fragmentManager is the fragment manager that will keep each fragment's state in the adapter
+     *                        across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fragmentManager) {
         super(fragmentManager);
+
+        mContextFragment = context;
     }
 
     @Override
@@ -28,5 +43,22 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContextFragment.getString(R.string.category_numbers);
+            case 1:
+                return mContextFragment.getString(R.string.category_family);
+            case 2:
+                return mContextFragment.getResources().getString(R.string.category_colors);
+            case 3:
+                return mContextFragment.getResources().getString(R.string.category_phrases);
+            default:
+                return null;
+        }
     }
 }
