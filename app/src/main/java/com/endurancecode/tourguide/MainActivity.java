b64a57b7136d52a2a70c690403566b5ccbae7158
EnduranceCode <endurancecode@gmail.com>
2018-11-16 17:53:40 +0000

    Set MainActivity layout

--- app/src/main/java/com/endurancecode/tourguide/MainActivity.java
+++ app/src/main/java/com/endurancecode/tourguide/MainActivity.java
@@ -1,5 +1,12 @@
 package com.endurancecode.tourguide;
 
+/* TODO: Set cards to open specific Fragment
+ * https://stackoverflow.com/questions/36063704/how-to-launch-activity-and-show-specific-fragment
+ * https://stackoverflow.com/questions/45664450/choosing-particular-fragment-to-display-in-activity
+ * https://stackoverflow.com/questions/43178728/how-to-intent-into-specific-tab-fragment
+ * https://stackoverflow.com/questions/33627106/how-to-open-a-specific-fragment-page-in-viewpager-from-another-activity-button-c
+ */
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
