0c897ff4c4b79f189962c519e05309ab92246d87
EnduranceCode <endurancecode@gmail.com>
2018-12-06 22:19:14 +0000

    Add loader to run the HTTP request in a background thread

---
+++ app/src/main/java/com/endurancecode/NewsAppStageOne/NewsLoader.java
@@ -0,0 +1,51 @@
+package com.endurancecode.NewsAppStageOne;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.util.List;
+
+/**
+ * Loads a list of news by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    /* Tag for log messages */
+    private static final String LOG_TAG = NewsLoader.class.getName();
+
+    /* Declares the variable to store the url to request */
+    private String urlRequest;
+
+    /**
+     * Constructs a new {@link News}.
+     *
+     * @param context of the activity
+     * @param url     to load data from
+     */
+    public NewsLoader(@NonNull Context context, String url) {
+        super(context);
+
+        urlRequest = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public List<News> loadInBackground() {
+
+        /* Don't perform the request if URL is null. */
+        if (urlRequest == null) {
+            return null;
+        }
+
+        /* Perform the network request, parse the response, and extract a list of earthquakes. */
+        return FetchUtils.fetchNewsData(urlRequest);
+    }
+}
