5ed3e111c32381e80a1e497d156eb8dd888d15a5
EnduranceCode <endurancecode@gmail.com>
2018-12-07 16:28:34 +0000

    Make a preferences screen

--- app/src/main/java/com/endurancecode/NewsAppStageTwo/MainActivity.java
+++ app/src/main/java/com/endurancecode/NewsAppStageTwo/MainActivity.java
@@ -10,6 +10,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -151,5 +153,27 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     private void hideLoadingSpinner() {
         ProgressBar loadingSpinner = findViewById(R.id.loading_spinner);
         loadingSpinner.setVisibility(View.GONE);
+    }
+
+    /* This method initialize the contents of the Activity's options menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Inflate the Options Menu we specified in XML */
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /* This method is called whenever an item in the options menu is selected */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/endurancecode/NewsAppStageTwo/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.endurancecode.NewsAppStageTwo;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static class NewsPreferencesFragment extends PreferenceFragment {
+
+    }
+}
