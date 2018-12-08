d55e69bc72a0f6b33490fa45534058cdbeb61a29
EnduranceCode <endurancecode@gmail.com>
2018-12-08 23:07:18 +0000

    Implement Preference Summaries

--- app/src/main/java/com/endurancecode/NewsAppStageTwo/SettingsActivity.java
+++ app/src/main/java/com/endurancecode/NewsAppStageTwo/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.endurancecode.NewsAppStageTwo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -14,12 +17,33 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @SuppressWarnings("deprecation")
-    public static class NewsPreferencesFragment extends PreferenceFragment {
+    public static class NewsPreferencesFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference pageSize = findPreference(getString(R.string.settings_page_size_key));
+            bindPreferenceSummaryToValue(pageSize);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+            /* The code in this method takes care of updating the displayed preference summary
+             * after it has been changed
+             */
+            String settingsPageSizeValue = newValue.toString();
+            preference.setSummary(settingsPageSizeValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
