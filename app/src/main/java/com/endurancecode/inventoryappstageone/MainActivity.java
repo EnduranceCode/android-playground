9cd118ee8a69522d7537cb30b0cad9697c4cab0a
EnduranceCode <endurancecode@gmail.com>
2018-12-12 16:41:13 +0000

    Add method to insert data in the database

--- app/src/main/java/com/endurancecode/inventoryappstageone/MainActivity.java
+++ app/src/main/java/com/endurancecode/inventoryappstageone/MainActivity.java
@@ -1,9 +1,11 @@
 package com.endurancecode.inventoryappstageone;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.endurancecode.inventoryappstageone.data.InventoryContract.Products;
 import com.endurancecode.inventoryappstageone.data.ProductDatabaseHelper;
@@ -18,6 +20,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Method to insert data in the database
+     *
+     * @param productName   name of the product
+     * @param price         price of the product
+     * @param quantity      quantity in stock of the product
+     * @param supplier      product's supplier
+     * @param supplierPhone product's supplier phone number
+     */
+    private void insertData(String productName, double price, int quantity, String supplier, String supplierPhone) {
+
+        /* Create database helper */
+        productDatabase = new ProductDatabaseHelper(this);
+
+        /* Gets the database in write mode */
+        SQLiteDatabase sqLiteDatabase = productDatabase.getWritableDatabase();
+
+        /*
+         * Create a ContentValues object where column names are the keys,
+         * and pet attributes from the editor are the values.
+         */
+        ContentValues values = new ContentValues();
+        values.put(Products.PRODUCT_NAME, productName);
+        values.put(Products.PRICE, price);
+        values.put(Products.QUANTITY, quantity);
+        values.put(Products.SUPPLIER, supplier);
+        values.put(Products.SUPPLIER_PHONE, supplierPhone);
+
+        /* Insert a new product in the database, returning the ID of that new row */
+        long newRowID = sqLiteDatabase.insert(Products.TABLE_NAME, null, values);
+
+        /* Show a toast message depending on whether or not the insertion was successful */
+        if (newRowID == -1) {
+            Toast.makeText(this, "Error with inserting the new product", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Produto inserido na linha " + newRowID, Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
