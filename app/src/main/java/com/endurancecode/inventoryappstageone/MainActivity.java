c8e2ddff8ca8e6d6a0995381573ee59647c01881
EnduranceCode <endurancecode@gmail.com>
2018-12-12 16:51:31 +0000

    Add temporary call to insertData() method to test it

--- app/src/main/java/com/endurancecode/inventoryappstageone/MainActivity.java
+++ app/src/main/java/com/endurancecode/inventoryappstageone/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Temporary method call to test the insertData() method */
+        insertData("Test Product", 12.34, 1, "EnduranceCode", "243 000 000");
     }
 
     /**
