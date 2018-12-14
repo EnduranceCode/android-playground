15ea8e0c7e87c9ea583689c5f612fbbe9ce0f019
EnduranceCode <endurancecode@gmail.com>
2018-12-14 11:50:06 +0000

    Use the ContentProvider Query() Method

--- app/src/main/java/com/endurancecode/pets/CatalogActivity.java
+++ app/src/main/java/com/endurancecode/pets/CatalogActivity.java
@@ -92,15 +92,10 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_GENDER,
                 PetEntry.COLUMN_PET_WEIGHT};
 
-        /* Perform a query on the pets table */
-        Cursor cursor = db.query(
-                PetEntry.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                null);
+        /*
+         * Perform a query on provider using the ContentResolver
+         * Use the {@link PetEntry#CONTENT_URI} to access the pet data */
+        Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI, projection, null, null, null);
 
         TextView displayView = (TextView) findViewById(R.id.text_view_pet);
 
