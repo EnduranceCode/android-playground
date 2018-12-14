2c75df5a9ab417cd05eae7996fe68ac0355eaa4d
EnduranceCode <endurancecode@gmail.com>
2018-12-14 10:10:21 +0000

    Add URIs to the Contract

--- app/src/main/java/com/endurancecode/pets/data/PetContract.java
+++ app/src/main/java/com/endurancecode/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.endurancecode.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -14,10 +15,39 @@ public final class PetContract {
     private PetContract() {
     }
 
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.endurancecode.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
     /* Inner class that defines the table contents */
     public static class PetEntry implements BaseColumns {
 
-        /* Name of the database table for pets */
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+        /**
+         * Name of the database table for pets
+         */
         public static final String TABLE_NAME = "pets";
 
         /**
