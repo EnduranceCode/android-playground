d29da33acbc91be794a563562dae5a6d55041c79
EnduranceCode <endurancecode@gmail.com>
2018-12-16 13:17:39 +0000

    Implement a CursorAdapter

--- app/src/main/java/com/endurancecode/pets/CatalogActivity.java
+++ app/src/main/java/com/endurancecode/pets/CatalogActivity.java
@@ -19,27 +19,21 @@ package com.endurancecode.pets;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.endurancecode.pets.data.PetContract.PetEntry;
-import com.endurancecode.pets.data.PetDbHelper;
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
 public class CatalogActivity extends AppCompatActivity {
-
-    /* Database helper that will provide us access to the database */
-    private PetDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,14 +49,6 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        /*
-         * To access our database, we instantiate our subclass of SQLiteOpenHelper
-         * and pass the context, which is the current activity.
-         */
-        mDbHelper = new PetDbHelper(this);
-
-        displayDatabaseInfo();
     }
 
     @Override
@@ -78,9 +64,6 @@ public class CatalogActivity extends AppCompatActivity {
      * the pets database.
      */
     private void displayDatabaseInfo() {
-
-        // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         /*
          * Define a projection that specifies which columns from the database
@@ -98,57 +81,14 @@ public class CatalogActivity extends AppCompatActivity {
          * Use the {@link PetEntry#CONTENT_URI} to access the pet data */
         Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI, projection, null, null, null);
 
-        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+        /* Find ListView to populate */
+        ListView petsListView = findViewById(R.id.list);
 
-        try {
-            /*
-             * Create a header in the Text View that looks like this:
-             *
-             * The pets table contains <number of rows in Cursor> pets.
-             * _id - name - breed - gender - weight
-             *
-             * In the while loop below, iterate through the rows of the cursor and display
-             * the information from each column in this order.
-             */
-            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetEntry._ID + " - " +
-                    PetEntry.COLUMN_PET_NAME + " - " +
-                    PetEntry.COLUMN_PET_BREED + " - " +
-                    PetEntry.COLUMN_PET_GENDER + " - " +
-                    PetEntry.COLUMN_PET_WEIGHT + "\n");
+        /* Setup cursor adapter using cursor from last step */
+        PetCursorAdapter petCursorAdapter = new PetCursorAdapter(this, cursor);
 
-            /* Figure out the index of each column */
-            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
-            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
-            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
-
-            /* Iterate through all the returned rows in the cursor */
-            while (cursor.moveToNext()) {
-                /*
-                 * Use that index to extract the String or Int value of the word
-                 * at the current row the cursor is on.
-                 */
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                String currentBreed = cursor.getString(breedColumnIndex);
-                int currentGender = cursor.getInt(genderColumnIndex);
-                int currentWeight = cursor.getInt(weightColumnIndex);
-                /* Display the values from each column of the current row in the cursor in the TextView */
-                displayView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        currentBreed + " - " +
-                        currentGender + " - " +
-                        currentWeight + " - "));
-            }
-        } finally {
-            /*
-             * Always close the cursor when you're done reading from it. This releases all its
-             * resources and makes it invalid.
-             */
-            cursor.close();
-        }
+        /* Attach cursor adapter to the ListView */
+        petsListView.setAdapter(petCursorAdapter);
     }
 
     /**
