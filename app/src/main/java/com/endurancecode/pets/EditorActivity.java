c03d41932000e54a3f6efc0eac4dc339d3bf0ea1
EnduranceCode <endurancecode@gmail.com>
2018-12-18 09:45:13 +0000

    Delete Pet From EditorActivity

--- app/src/main/java/com/endurancecode/pets/EditorActivity.java
+++ app/src/main/java/com/endurancecode/pets/EditorActivity.java
@@ -223,9 +223,9 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         values.put(PetEntry.COLUMN_PET_GENDER, mGender);
 
         /*
-        If the weight is not provided by the user, don't try to parse the string into an
-        integer value. Use 0 by default
-        */
+         * If the weight is not provided by the user, don't try to parse the string into an
+         * integer value. Use 0 by default
+         */
         int weight = 0;
         if (!TextUtils.isEmpty(stringWeight)) {
             weight = Integer.parseInt(stringWeight);
@@ -306,7 +306,8 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
                 return true;
             /* Respond to a click on the "Delete" menu option */
             case R.id.action_delete:
-                // Do nothing for now
+                /* Pop up confirmation dialog for deletion */
+                showDeleteConfirmationDialog();
                 return true;
             /* Respond to a click on the "Up" arrow button in the app bar */
             case android.R.id.home:
@@ -366,6 +367,58 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
         /* Show dialog that there are unsaved changes */
         showUnsavedChangesDialog(discardButtonClickListener);
+    }
+
+    /**
+     * Prompt the user to confirm that they want to delete this pet.
+     */
+    private void showDeleteConfirmationDialog() {
+        /*
+         * Create an AlertDialog.Builder and set the message, and click listeners
+         * for the positive and negative buttons on the dialog.
+         */
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                /* User clicked the "Delete" button, so delete the pet */
+                deletePet();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        /* Create and show the AlertDialog */
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    /**
+     * Perform the deletion of the pet in the database.
+     */
+    private void deletePet() {
+        /* Only perform the delete if this is an existing pet */
+        if (mCurrentPetUri != null) {
+
+            int rowsDeleted = getContentResolver().delete(mCurrentPetUri, null, null);
+
+            /* Show a toast message depending on whether or not the update was successful */
+            if (rowsDeleted == 0) {
+                Toast.makeText(this, getString(R.string.editor_delete_pet_failed), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getString(R.string.editor_delete_pet_successful), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        /* Close the activity */
+        finish();
     }
 
     @NonNull
